366cca1e67c0a59bfbf4ecf438ba066e37d19d84
Edmarie <42824021+edmariemarr@users.noreply.github.com>
2024-12-08 23:18:26 -0400

    mod

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,26 @@
   -------------------------------------------------------------------------------*/
 
   $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(1000);   
   });
 
+  /*-------------------------------------------------------------------------------
+    FADE ITEMS
+  -------------------------------------------------------------------------------*/
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); 
+      }
+  });
+}, {
+  threshold: 0.5 
+});
+
+const fadeElements = document.querySelectorAll('.fade-in');
+
+fadeElements.forEach(element => {
+  observer.observe(element);
+});
